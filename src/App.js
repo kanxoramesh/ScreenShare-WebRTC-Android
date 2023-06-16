63ae6cc7a58fb7ea40fd3191ef7b999a989929db
ramesh pokhrel <kanxoramesh@gmail.com>
2023-06-16 22:07:49 +0545

    firebase config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const firestore = firebase.firestore();
+
+// Initialize WebRTC
+const servers = {
+  iceServers: [
+      {
+          urls: [
+              "stun:stun1.l.google.com:19302",
+              "stun:stun2.l.google.com:19302",
+          ],
+      },
+  ],
+  iceCandidatePoolSize: 10,
+};
+const pc = new RTCPeerConnection(servers);
+
 
 function App() {
   return (
