d77256fee09c31765367b2e99d8e4885582d67ca
ramesh pokhrel <kanxoramesh@gmail.com>
2023-06-18 18:29:54 +0545

    handle move

--- src/Remote.js
+++ src/Remote.js
@@ -34,6 +34,8 @@ const servers = {
     iceCandidatePoolSize: 10,
 };
 const pc = new RTCPeerConnection(servers);
+const dataChannel = pc.createDataChannel("channel");
+
 const firestore = firebase.firestore();
 pc.addTransceiver('video');
 const remoteControl = firestore.collection("remoteControl");
@@ -51,6 +53,8 @@ function Remote() {
     const [loading, setLoading] = useState(false);
     const [remoteId, setRemoteId] = useState("");
     const [error, setError] = useState(false);
+    const [videoWidth, setVideoWidth] = useState(0);
+    const [videoHeight, setVideoHeight] = useState(0);
 
 
     async function clearCollection(ref) {
@@ -62,11 +66,12 @@ function Remote() {
     }
 
     const setStatus = async () => {
+        await clearCollection(myOffer)
+        await clearCollection(myiceCandidates)
+        await clearCollection(remoteControl.doc(remoteId).collection("answer"))
+        await clearCollection(remoteControl.doc(remoteId).collection("iceCandidates"))
         myDoc.set({ "status": true })
         //clear previous
-        // await clearCollection(myOffer)
-        // await clearCollection(myiceCandidates)
-
     }
     const setRequestToCallee = () => {
         remoteControl.doc(remoteId).set({
@@ -79,8 +84,21 @@ function Remote() {
             const data = snapshot.data();
             if (data?.status) {
                 //update video width height
-                var dWidth = data?.dWidth;
-                var dHeight = data?.dHeight;
+                var dWidth = data?.dWidth;//1080
+                var dHeight = data?.dHeight;//2260
+
+                setVideoHeight(dHeight)
+                setVideoWidth(dWidth)
+
+                // if (localRef.current) {
+                //     const { videoWidth, videoHeight } = localRef.current;
+
+
+                //     const scalingFactorX = dWidth / videoWidth;
+                //     const scalingFactorY = dHeight / videoHeight;
+
+
+                // }
 
                 //need to send offer
                 setIceAndOfferCandidates()
@@ -144,7 +162,15 @@ function Remote() {
                 }
             });
         });
+        dataChannel.addEventListener('open', event => {
+           console.log("open channel")
+        });
+        
+        // Disable input when closed
+        dataChannel.addEventListener('close', event => {
+            console.log("close channel")
 
+        });
 
     }
 
@@ -162,7 +188,8 @@ function Remote() {
         }
         SetConnect(false)
         setLoading(false)
-
+        await clearCollection(myOffer)
+        await clearCollection(myiceCandidates)
         remoteControl.doc(remoteId).set({
         })
         myDoc.update({ "status": false })
@@ -215,6 +242,7 @@ function Remote() {
 
     const handleMouseDown = (event) => {
         setIsDown(true)
+        dataChannel.send(`${event.nativeEvent.offsetX}:${event.nativeEvent.offsetY}:ACTION_DOWN`);
     };
 
     const handleMouseMove = (event) => {
@@ -223,13 +251,18 @@ function Remote() {
             setIsDragging(true);
             console.log("drag", event.nativeEvent.offsetX, event.nativeEvent.offsetY)
             //sen event
+            dataChannel.send(`${event.nativeEvent.offsetX}:${event.nativeEvent.offsetY}:ACTION_MOVE`);
         }
     };
 
     const handleMouseUp = (event) => {
         if (!isDragging) {
             console.log("click", event.nativeEvent.offsetX, event.nativeEvent.offsetY)
+            dataChannel.send(`${event.nativeEvent.offsetX}:${event.nativeEvent.offsetY}:ACTION_CLICK`);
+
         }
+        dataChannel.send(`${event.nativeEvent.offsetX}:${event.nativeEvent.offsetY}:ACTION_UP`);
+
         setIsDragging(false);
         setIsDown(false)
 
@@ -275,6 +308,8 @@ function Remote() {
                         playsInline
                         className="local"
                         muted
+                        style={{ width: videoWidth, height: videoHeight }}
+
                     />
                 </div>
 
@@ -283,206 +318,6 @@ function Remote() {
     )
 }
 
-
-
-function Videos({ mode, callId, setPage }) {
-    const [webcamActive, setWebcamActive] = useState(false);
-    const [roomId, setRoomId] = useState(callId);
-
-    const localRef = useRef();
-    const remoteRef = useRef();
-
-    const setupSources = async () => {
-        const localStream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true,
-        });
-        const remoteStream = new MediaStream();
-
-        localStream.getTracks().forEach((track) => {
-            pc.addTrack(track, localStream);
-        });
-
-        pc.ontrack = (event) => {
-            event.streams[0].getTracks().forEach((track) => {
-                remoteStream.addTrack(track);
-            });
-        };
-
-        localRef.current.srcObject = localStream;
-        remoteRef.current.srcObject = remoteStream;
-
-        setWebcamActive(true);
-
-        if (mode === "create") {
-            const callDoc = firestore.collection("calls").doc();
-            const offerCandidates = callDoc.collection("offerCandidates");
-            const answerCandidates = callDoc.collection("answerCandidates");
-
-            setRoomId(callDoc.id);
-
-            pc.onicecandidate = (event) => {
-                event.candidate &&
-                    offerCandidates.add(event.candidate.toJSON());
-            };
-
-            const offerDescription = await pc.createOffer();
-            await pc.setLocalDescription(offerDescription);
-
-            const offer = {
-                sdp: offerDescription.sdp,
-                type: offerDescription.type,
-            };
-
-            await callDoc.set({ offer });
-
-            callDoc.onSnapshot((snapshot) => {
-                const data = snapshot.data();
-                if (!pc.currentRemoteDescription && data?.answer) {
-                    const answerDescription = new RTCSessionDescription(
-                        data.answer
-                    );
-                    pc.setRemoteDescription(answerDescription);
-                }
-            });
-
-            answerCandidates.onSnapshot((snapshot) => {
-                snapshot.docChanges().forEach((change) => {
-                    if (change.type === "added") {
-                        const candidate = new RTCIceCandidate(
-                            change.doc.data()
-                        );
-                        pc.addIceCandidate(candidate);
-                    }
-                });
-            });
-        } else if (mode === "join") {
-            const callDoc = firestore.collection("calls").doc(callId);
-            const answerCandidates = callDoc.collection("answerCandidates");
-            const offerCandidates = callDoc.collection("offerCandidates");
-
-            pc.onicecandidate = (event) => {
-                event.candidate &&
-                    answerCandidates.add(event.candidate.toJSON());
-            };
-
-            const callData = (await callDoc.get()).data();
-
-            const offerDescription = callData.offer;
-            await pc.setRemoteDescription(
-                new RTCSessionDescription(offerDescription)
-            );
-
-            const answerDescription = await pc.createAnswer();
-            await pc.setLocalDescription(answerDescription);
-
-            const answer = {
-                type: answerDescription.type,
-                sdp: answerDescription.sdp,
-            };
-
-            await callDoc.update({ answer });
-
-            offerCandidates.onSnapshot((snapshot) => {
-                snapshot.docChanges().forEach((change) => {
-                    if (change.type === "added") {
-                        let data = change.doc.data();
-                        pc.addIceCandidate(new RTCIceCandidate(data));
-                    }
-                });
-            });
-        }
-
-        pc.onconnectionstatechange = (event) => {
-            if (pc.connectionState === "disconnected") {
-                hangUp();
-            }
-        };
-    };
-
-    const hangUp = async () => {
-        pc.close();
-
-        if (roomId) {
-            let roomRef = firestore.collection("calls").doc(roomId);
-            await roomRef
-                .collection("answerCandidates")
-                .get()
-                .then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        doc.ref.delete();
-                    });
-                });
-            await roomRef
-                .collection("offerCandidates")
-                .get()
-                .then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        doc.ref.delete();
-                    });
-                });
-
-            await roomRef.delete();
-        }
-
-        window.location.reload();
-    };
-
-    return (
-        <div className="videos">
-            <video
-                ref={localRef}
-                autoPlay
-                playsInline
-                className="local"
-                muted
-            />
-
-            <video ref={remoteRef} autoPlay playsInline className="remote" />
-
-            <div className="buttonsContainer">
-                <button
-                    onClick={hangUp}
-                    disabled={!webcamActive}
-                    className="hangup button"
-                >
-                    <HangupIcon />
-                </button>
-                <div tabIndex={0} role="button" className="more button">
-                    <div className="popover">
-                        <button
-                            onClick={() => {
-                                navigator.clipboard.writeText(roomId);
-                            }}
-                        >
-                            Copy joining code
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            {!webcamActive && (
-                <div className="modalContainer">
-                    <div className="modal">
-                        <h3>
-                            Turn on your camera and microphone and start the
-                            call
-                        </h3>
-                        <div className="container">
-                            <button
-                                onClick={() => setPage("home")}
-                                className="secondary"
-                            >
-                                Cancel
-                            </button>
-                            <button onClick={setupSources}>Start</button>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
 
 
 export default Remote;
